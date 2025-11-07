f8758c5f9b33f5ea681cc95b26250295b7196c05
Elvis <elviskb@gmail.com>
2025-11-07 13:45:33 +0300

    update CORS to fix a bug

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ const PORT = process.env.PORT || 5000
 app.use('/uploads', express.static(path.join(process.cwd(), 'public', 'uploads')));
 
 
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+  origin: [process.env.FRONTEND_URL],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
